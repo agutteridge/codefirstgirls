94868d2a1c8bbaa1be3f9236503d0875688b1a4d
Alice Gutteridge <alicegutteridge@gmail.com>
2018-11-29 23:12:46 +0000

    indentation and requests

--- deploytest.py
+++ deploytest.py
@@ -9,20 +9,20 @@ def hello():
 @app.route('/films/<number>')
 def films(number):
     all_films = {
-    	'1': 'The Phantom Menace',
-    	'2': 'The Empire Strikes Back',
-    	'3': 'Revenge of the Sith',
-    	'4': 'A New Hope',
-    	'5': 'The Empire Strikes Back',
-    	'6': 'Return of the Jedi',
+        '1': 'The Phantom Menace',
+        '2': 'The Empire Strikes Back',
+        '3': 'Revenge of the Sith',
+        '4': 'A New Hope',
+        '5': 'The Empire Strikes Back',
+        '6': 'Return of the Jedi',
         '7': 'The Force Awakens',
         '8': 'The Last Jedi'
     }
 
     if number in all_films:
-    	return render_template('starwars.html', title=all_films[number])
+        return render_template('starwars.html', title=all_films[number])
     else:
-    	return render_template('404.html'), 404
+        return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
